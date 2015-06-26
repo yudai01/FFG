058f659396d1eb8a0396a90fa39dfc07180ecb70
moko2 <motoyamakouki@motoyama-no-MacBook-Air.local>
2015-06-27 01:35:09 +0900

    Mainにコマンドラインオプションを解析する処理を追加する
    Main クラスにコマンドラインオプションを解析する処理（parseArguments(String[] arguments)）を追加する．
    parseArguments メソッドをコンストラクタから呼び出す．

--- src/main/resouces/main.java
+++ src/main/resouces/main.java
@@ -2,8 +2,18 @@ package com.github.finder;
 
 public class Main{
     public Main(String[] argments){
-    }
+	new parseArguments(argments); 
+   }
     public static void main(String[] args){
 	new Main(args);
+    }
+    private Args parseArguments(String[] arguments){
+        Args args = new Args();
+        try {
+            CmdLineParser parser = new CmdLineParser(args);
+            parser.parseArgument(arguments);
+        } catch (CmdLineException e) {
+        }
+        return args;
     }
 }
